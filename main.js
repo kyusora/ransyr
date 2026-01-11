56c9063430633c409b03d94dcf113067faf56587
kyusora <lynnsynth3@gmail.com>
2026-01-11 13:30:45 +0900

    update

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
 
-export default class ransyr {
+
+export default class randx {
+	
+	static Rest;
+	
+	static randomseed(seed) {
+		let m = 0x80000000;
+		let a = 1103515245;
+		let c = 12345;
+		let state = seed ? seed : Math.floor(Math.random() * (m-1));
+		state = (a * state + c) % m;
+		randx.Rest = state / (m - 1);
+	}
 
 	static random() {
-		return Math.random();
+		return randx.Rest ? randx.Rest : Math.random();
 	}
 
 	static sample(arr, k) {
 		if (k > arr.length) {
-			throw new RangeError("sample size larger than population");
+			throw new RangeError("insufficient array length");
 		}
 
 		const copy = [...arr];
 		const result = [];
 
 		for (let i = 0; i < k; i++) {
-			const idx = Math.floor(Math.random() * copy.length);
+			const idx = Math.floor(randx.random() * copy.length);
 			result.push(copy[idx]);
 			copy.splice(idx, 1);
 		}
@@ -26,18 +38,18 @@ export default class ransyr {
 		if (min > max) [min,
 			max] = [max,
 			min];
-		return Math.floor(Math.random() * (max - min + 1)) + min;
+		return Math.floor(randx.random() * (max - min + 1)) + min;
 	}
 
 	static choice(arr) {
 		if (!arr.length) return undefined;
-		return arr[Math.floor(Math.random() * arr.length)];
+		return arr[Math.floor(randx.random() * arr.length)];
 	}
 
 	static shuffle(arr) {
 		const copy = [...arr];
 		for (let i = copy.length - 1; i > 0; i--) {
-			const j = Math.floor(Math.random() * (i + 1));
+			const j = Math.floor(randx.random() * (i + 1));
 			[copy[i],
 				copy[j]] = [copy[j],
 				copy[i]];
@@ -46,7 +58,6 @@ export default class ransyr {
 	}
 
 	static chance(percent) {
-		return Math.random() * 100 < percent;
+		return randx.random() * 100 < percent;
 	}
 }
-
